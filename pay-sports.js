cb247222797ff5e0d5763880ec0ba3168b31b30b
Robert Fischer <robert.fischer@smokejumperit.com>
2013-02-13 18:47:35 -0500

    Add the stylesheet

--- pay-sports.js
+++ pay-sports.js
@@ -1,2 +1,3 @@
 document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="pay-sports.css">');
 document.write('<script src="pay-sports-code.js"></script>');
