f62c806a929d1e857207707715653bcd6b2775e3
Robert Fischer <robert.fischer@smokejumperit.com>
2013-02-13 21:25:13 -0500

    Okay, not quite that long on the timeouts

--- pay-sports-code.js
+++ pay-sports-code.js
@@ -54,11 +54,11 @@ jQuery(function($) {
       if((old_amount+0.01) <= amount) {
         $('.league_amount', block).text(number_to_dollars(old_amount+0.01));
         block.data('amount', old_amount+0.01);
-        setTimeout(adjust, 1000);
+        setTimeout(adjust, 100);
       } else if ((old_amount-0.01) >= amount) {
         $('.league_amount', block).text(number_to_dollars(Math.max(0, old_amount-0.01)));
         block.data('amount', Math.max(0,old_amount-0.01));
-        setTimeout(adjust, 1000);
+        setTimeout(adjust, 100);
       } else {
         block.fadeIn('slow');
       }
