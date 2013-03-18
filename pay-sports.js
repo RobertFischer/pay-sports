9385b59f0a8abc01d22672324bf467d0d0731773
Robert Fischer <robert.fischer@smokejumperit.com>
2013-03-18 16:08:22 -0400

    Made it easier to change the domain where the file is deployed

--- pay-sports.js
+++ pay-sports.js
@@ -1,3 +1,7 @@
-document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>');
-document.write('<link rel="stylesheet" type="text/css" href="pay-sports.css">');
-document.write('<script src="pay-sports-code.js"></script>');
+(function() {
+	var domain = "http://paysports.smokejumperit.com";
+
+	document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>');
+	document.write('<link rel="stylesheet" type="text/css" href="' + domain + '/pay-sports.css">');
+	document.write('<script src="' + domain + '/pay-sports-code.js"></script>');
+})();
