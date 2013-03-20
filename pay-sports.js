5f33aa53b677d5acc3cf707b598aaa8eff800619
Robert Fischer <robert.fischer@smokejumperit.com>
2013-03-20 13:37:07 -0400

    Check in the common domain cases we want with other versions commented out

--- pay-sports.js
+++ pay-sports.js
@@ -1,6 +1,10 @@
 (function() {
-	var domain = window.location.pathname;  // Set to the domain and/or path where the files below live
-																					// e.g. "http://mydomain.com/pay-sports" or "/pay-sports"
+	// Set to the domain and/or path where the files below live
+	// e.g. "http://mydomain.com/pay-sports" or "/pay-sports"
+	var domain = window.location.pathname;  
+	// var domain = "http://www.whatyoupayforsports.com/code/";
+	// var domain = "/code/";
+	// var domain = "file:///Users/robert/wkdir/pay-sports/";
 
 	document.write('<link rel="stylesheet" type="text/css" href="' + domain + 'pay-sports.css">');
 	document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>');
